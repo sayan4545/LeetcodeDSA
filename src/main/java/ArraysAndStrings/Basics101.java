5f194f2e47d66e48a7d0de1c5cee7fd5288fc836
sayan4545 <sayanchatterjee3010@gmail.com>
2025-02-24 00:58:10 +0530

    First commit

---
+++ src/main/java/ArraysAndStrings/Basics101.java
@@ -0,0 +1,17 @@
+package ArraysAndStrings;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Basics101 {
+    public static void main(String[] args) {
+        // declaring an array
+        int [] rnos ;
+        // This is declaring the variable in the stack. dataype is integer.
+        // datatype defines the type of the items we can store in the array.
+        // array is collection of datatypes.
+        rnos = new int[10];
+        // This is defining or creating the object in the heap memory.size is pre defined.
+
+    }
+}
