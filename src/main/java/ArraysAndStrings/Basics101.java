d70448d339b49bbb2609f601aaea3e2b0c73e6dd
sayan4545 <sayanchatterjee3010@gmail.com>
2025-02-24 01:50:20 +0530

    Day 01--> Arrays and Strings Theoruy + Leetcode questions.

--- src/main/java/ArraysAndStrings/Basics101.java
+++ src/main/java/ArraysAndStrings/Basics101.java
@@ -12,6 +12,14 @@ public class Basics101 {
         // array is collection of datatypes.
         rnos = new int[10];
         // This is defining or creating the object in the heap memory.size is pre defined.
-
+        // The decalaration thing happens at compile time, and the object creation in the heap memory happens
+        // at run time
+        // inn C++, arrays are continuous memory location. There is no solid
+        //proof of this in java. It depends on the JVM
+        // there is 0 based indexing in arrays.
+        System.out.println(rnos[0]); // This will give 0 as o/p. Since Integer type.
+        // had it been a string type, it would have resulted in null.
+        String[] strArray = new String[6];
+        System.out.println(strArray[2]); // null
     }
 }
