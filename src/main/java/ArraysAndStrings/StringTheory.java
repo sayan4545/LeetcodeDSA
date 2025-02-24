25b289e79211f808c4a16787454337c1585578d5
sayan4545 <sayanchatterjee3010@gmail.com>
2025-02-24 21:15:27 +0530

    Started String

---
+++ src/main/java/ArraysAndStrings/StringTheory.java
@@ -0,0 +1,11 @@
+package ArraysAndStrings;
+
+public class StringTheory {
+    public static void main(String[] args) {
+        String name = "Sayan Chatterjee";
+        System.out.println(name);
+        String name2 = new String("Chandrika Dey");
+        System.out.println(name2);
+
+    }
+}
