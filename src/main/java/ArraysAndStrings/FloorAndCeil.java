96dfb855941d725480bcba3060c601b4753c6b3e
sayan4545 <sayanchatterjee3010@gmail.com>
2025-02-26 01:45:23 +0530

    Added ceil code

--- src/main/java/ArraysAndStrings/FloorAndCeil.java
+++ src/main/java/ArraysAndStrings/FloorAndCeil.java
@@ -2,10 +2,11 @@ package ArraysAndStrings;
 
 public class FloorAndCeil {
     public static void main(String[] args) {
-        int[] arr = {1,4,7,9};
-        System.out.println("The floor of 5 is : " + floor(arr,5));
+        int[] arr = {2,4,7,9};
+        System.out.println("The floor of 1 is : " + floor(arr,1));
         System.out.println("The floor of 7 is : " + floor(arr,7));
-
+        System.out.println("The ceil of 5 is : "+ ceil(arr,5));
+        System.out.println("The ceil of 10 is " + ceil(arr,10));
     }
     public static int floor(int[] num,int target){
         int s = 0;
@@ -22,5 +23,20 @@ public class FloorAndCeil {
         }
         return ans;
 
+    }
+    public static int ceil(int[] num,int target){
+        int s = 0;
+        int e = num.length-1;
+        int ans = -1;
+        while(s<=e){
+            int m = s+(e-s)/2;
+            if(num[m]>=target){
+                ans = num[m];
+                e= m-1;
+            }else{
+                s = m+1;
+            }
+        }
+        return ans;
     }
 }
