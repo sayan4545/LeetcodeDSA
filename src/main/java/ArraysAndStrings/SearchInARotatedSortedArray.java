3e1551400439e0e1a030052fcc842d1955150ab6
sayan4545 <sayanchatterjee3010@gmail.com>
2025-02-28 02:34:15 +0530

    Q: Search ibn a rotated sorted array with duplicates, return true or false

--- src/main/java/ArraysAndStrings/SearchInARotatedSortedArray.java
+++ src/main/java/ArraysAndStrings/SearchInARotatedSortedArray.java
@@ -5,6 +5,10 @@ public class SearchInARotatedSortedArray {
         int[] arr = {6,7,8,9,0,1,2,3,4};
         System.out.println(findElement(arr,7));
         System.out.println(findTargetElement(arr,3));//7
+        int[] arr2 = {3,3,4,4,5,5,1,2,3};
+        System.out.println(findElementWithDuplicates(arr2,4));
+        System.out.println(findElementWithDuplicates(arr2,1));
+        System.out.println(findElementWithDuplicates(arr2,8));
 
 
     }
@@ -56,5 +60,32 @@ public class SearchInARotatedSortedArray {
 
         }
         return -1;
+    }
+    public static boolean findElementWithDuplicates(int[] nums, int target){
+        int s = 0;
+        int e = nums.length-1;
+        while(s<=e){
+            int m = s+(e-s)/2;
+            if(nums[m]==target) return true;
+            if(nums[s]==nums[m] && nums[m]==nums[e]){
+                e = e-1;
+                s = s+1;
+            }
+            else if(nums[s]<=nums[m]){
+                if(target>=nums[s] && target <=nums[m]){
+                    e = m-1;
+                }else{
+                    s = m+1;
+                }
+            }
+            else{
+                if(target>=nums[m] && target <= nums[e]){
+                    s = m+1;
+                }else{
+                    e = m-1;
+                }
+            }
+        }
+        return false;
     }
 }
